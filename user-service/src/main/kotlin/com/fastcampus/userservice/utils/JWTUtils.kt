de3083ac7f4eb8d363b2f0757286258f560a95b2
sunghoon.lee <sunghoon.lee@myrealtrip.com>
2024-05-14 00:57:44 +0900

    jwt 인증기반 구현

---
+++ user-service/src/main/kotlin/com/fastcampus/userservice/utils/JWTUtils.kt
@@ -0,0 +1,38 @@
+package com.fastcampus.userservice.utils
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import com.auth0.jwt.interfaces.DecodedJWT
+import com.fastcampus.userservice.config.JWTProperties
+import java.util.*
+
+object JWTUtils {
+    fun createToken(claim: JWTClaim, properties: JWTProperties) =
+        JWT.create()
+            .withIssuer(properties.issuer)
+            .withSubject(properties.subject)
+            .withIssuedAt(Date())
+            .withExpiresAt(Date(Date().time + properties.expiresTime * 1000))
+            .withClaim("userId", claim.userId)
+            .withClaim("email", claim.email)
+            .withClaim("profileUrl", claim.profileUrl)
+            .withClaim("username", claim.username)
+            .sign(Algorithm.HMAC256(properties.secret))
+
+    fun decode(token: String, secret: String, issuer:String) : DecodedJWT {
+        val algorithm = Algorithm.HMAC256(secret)
+
+        val verifyer = JWT.require(algorithm)
+            .withIssuer(issuer)
+            .build()
+
+        return verifyer.verify(token)
+    }
+}
+
+data class JWTClaim(
+    var userId: Long,
+    val email: String,
+    val profileUrl: String,
+    val username: String,
+)
